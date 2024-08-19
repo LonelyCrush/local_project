7f76022d3b6285afcbc00789589dc7022dd5e98b
leizf <Aaz17373510579@163.com>
2024-08-19 22:40:59 +0800

    项目初始化

---
+++ src/test/java/com/lzf/LocalProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
