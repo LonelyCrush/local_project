98d2013345a95d518bcc257a5de645348670da01
雷泽峰 <leizefeng@formssi.com>
2024-09-18 14:07:23 +0800

    代码新增：d240918 读入和写出

---
+++ src/main/java/com/lzf/local/d240918/read/User.java
@@ -0,0 +1,20 @@
+package com.lzf.local.d240918.read;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author leizefeng
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+  private String name;
+
+  private Integer age;
+
+  private String gender;
+}
