5ef686296de8f30d0beaccf0441dee18faaacb20
雷泽峰 <leizefeng@formssi.com>
2024-10-08 15:37:03 +0800

    代码新增：d241008 一种使用动态SQL实现分批次快速插入30万条数据的方法

--- src/main/java/com/lzf/d240819/mapper/StudentMapper.java
+++ src/main/java/com/lzf/d240819/mapper/StudentMapper.java
@@ -2,12 +2,14 @@ package com.lzf.d240819.mapper;
 
 import com.lzf.d240819.entity.Student;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import java.util.List;
 
 /**
  * @Entity com.lzf.d240819.entity.Student
  */
 public interface StudentMapper extends BaseMapper<Student> {
 
+  void batchAdd(List<Student> studentList);
 }
 
 
---
+++ src/main/java/com/lzf/local/d241008/controller/BatchAddStudentController.java
@@ -0,0 +1,23 @@
+package com.lzf.local.d241008.controller;
+
+import com.lzf.local.d241008.service.BatchAddStudentService;
+import javax.annotation.Resource;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author leizefeng
+ */
+@RestController
+@RequestMapping("/student")
+public class BatchAddStudentController {
+
+  @Resource
+  private BatchAddStudentService batchAddStudentService;
+
+  @PostMapping("/batchAdd")
+  public void batchAdd() {
+    batchAddStudentService.batchAdd();
+  }
+}
