44c2ba59b6fb05b117f56cc4daa624450bb7e8e4
雷泽峰 <leizefeng@formssi.com>
2024-10-12 16:45:36 +0800

    代码新增：d241012 String.format()方法

---
+++ src/main/java/com/lzf/local/d241012/Format.java
@@ -0,0 +1,11 @@
+package com.lzf.local.d241012;
+
+/**
+ * @author leizefeng
+ */
+public class Format {
+
+  public static void main(String[] args) {
+    System.out.println(String.format("%06d", 1));
+  }
+}
