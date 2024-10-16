3aa051822c63806665f95ba2436784c5cda706e7
雷泽峰 <leizefeng@formssi.com>
2024-10-16 09:22:26 +0800

    代码新增：d241016 全局唯一标识方法

---
+++ src/main/java/com/lzf/local/d241016/UniqueKey.java
@@ -0,0 +1,14 @@
+package com.lzf.local.d241016;
+
+import java.util.UUID;
+
+/**
+ * @author leizefeng
+ */
+public class UniqueKey {
+
+  public static void main(String[] args) {
+    String s = UUID.randomUUID().toString().replace("-", "") + System.currentTimeMillis();
+    System.out.println(s);
+  }
+}
