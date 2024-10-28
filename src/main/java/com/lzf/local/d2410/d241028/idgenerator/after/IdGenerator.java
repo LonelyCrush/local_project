1b080422f6da818136bed065f219419655b492b7
雷泽峰 <leizefeng@formssi.com>
2024-10-28 10:35:01 +0800

    代码新增：d241028 Id生成器的重构

---
+++ src/main/java/com/lzf/local/d2410/d241028/idgenerator/after/IdGenerator.java
@@ -0,0 +1,9 @@
+package com.lzf.local.d2410.d241028.idgenerator.after;
+
+/**
+ * @author leizefeng
+ */
+public interface IdGenerator {
+
+  String generate();
+}
---
+++ src/main/java/com/lzf/local/d2410/d241028/idgenerator/after/LogTraceIdGenerator.java
@@ -0,0 +1,8 @@
+package com.lzf.local.d2410.d241028.idgenerator.after;
+
+/**
+ * @author leizefeng
+ */
+public interface LogTraceIdGenerator extends IdGenerator {
+
+}
